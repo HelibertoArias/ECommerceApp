0d671e14fce95345187e58855ca31b52040d6268
HelibertoArias <helibertoarias@gmail.com>
2016-12-19 14:13:36 -0500

    - Adición de Clase IntanceLocator para binding menú. - Adición de Pages en el menú. - Adición MenuItemViewModel y MainViewModel para la asociación de Iconos, Menu, y Pages. - Edición de App para agregar Locator.

---
+++ ECommerceApp/ECommerceApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Properties
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+        #endregion
+
+        #region Contructors
+        public MainViewModel()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+            LoadMenu();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void LoadMenu()
+        {
+ 
+            AddItem("ic_action_product.png", "ProductsPage", "Productos");
+            AddItem("ic_action_customer.png", "CustomersPage", "Clientes");
+            AddItem("ic_action_order.png", "OrdersPage", "Pedidos");
+            AddItem("ic_action_delivery.png", "DeliveriesPage", "Entregas");
+            AddItem("ic_action_sync.png", "SyncPage", "Sincronizar");
+            AddItem("ic_action_setup.png", "SetupPage", "Configuración");
+            AddItem("ic_action_logout.png", "Logout", "Salir");
+
+        }
+
+        private void AddItem(string icon, string viewName, string title)
+        {
+            Menu.Add(new MenuItemViewModel(icon, viewName, title));
+        }
+        #endregion
+    }
+}
---
+++ ECommerceApp/ECommerceApp/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ECommerceApp.ViewModels
+{
+    public class MenuItemViewModel
+    { 
+
+        public MenuItemViewModel(string icon, string pageName, string title)
+        {
+            this.Icon = icon;
+            this.PageName = pageName;
+            this.Title = title;
+        }
+
+        public string Icon { get; set; }
+
+        public string Title { get; set; }
+
+        public string PageName { get; set; }
+
+    }
+}
